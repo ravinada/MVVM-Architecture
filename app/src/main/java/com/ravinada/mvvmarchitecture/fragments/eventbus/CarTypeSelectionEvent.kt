a4121b2d3c677f48d630c84f93eed502e025c61d
Ravi Nada <ravinada72@gmail.com>
2020-06-02 22:08:30 +0530

    eventBus added

---
+++ app/src/main/java/com/ravinada/mvvmarchitecture/fragments/eventbus/CarTypeSelectionEvent.kt
@@ -0,0 +1,6 @@
+package com.ravinada.mvvmarchitecture.fragments.eventbus
+
+import com.ravinada.mvvmarchitecture.base.BaseEventBusModel
+
+class UserEvent(var status: String) : BaseEventBusModel("") {
+}
