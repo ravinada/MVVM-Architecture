93aa66b806f8359ca4b5f3124ec4904c6f3ba905
Ravi Nada <ravinada72@gmail.com>
2020-06-03 20:14:53 +0530

    some changes

--- app/src/main/java/com/ravinada/mvvmarchitecture/MainHostFragment.kt
+++ app/src/main/java/com/ravinada/mvvmarchitecture/MainHostFragment.kt
@@ -31,7 +31,7 @@ class BlankFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank4, container, false)
+        return inflater.inflate(R.layout.activity_main, container, false)
     }
 
     companion object {
